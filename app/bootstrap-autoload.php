597186a966c276d2c27c5bedd46594cd19845259
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2025-05-08 23:44:40 +0000

    update Doc

--- app/bootstrap-autoload.php
+++ app/bootstrap-autoload.php
@@ -8,7 +8,7 @@
  *
  * @package   Creativity
  * @author    Benjamin Lu <benlumia007@gmail.com>
- * @copyright 2023. Benjamin Lu
+ * @copyright 2023 Benjamin Lu
  * @license   https://www.gnu.org/licenses/gpl-2.0.html
  * @link      https://luthemes.com/portfolio/creativity
  */
