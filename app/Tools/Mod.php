850a53a257539905c1de14bc08c0f17752e3bdf0
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2025-05-12 16:31:00 +0000

    changed copyright.

--- app/Tools/Mod.php
+++ app/Tools/Mod.php
@@ -6,7 +6,7 @@
  *
  * @package   Creativity
  * @author    Benjamin Lu <benlumia007@gmail.com>
- * @copyright 2024 Benjamin Lu
+ * @copyright 2023 Benjamin Lu
  * @license   https://www.gnu.org/licenses/gpl-2.0.html
  * @link      https://luthemes.com/portfolio/Creativity
  */
